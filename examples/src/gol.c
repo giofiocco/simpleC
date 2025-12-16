761332da4917b6e01d6f990435e9bb793f0e6663
giofiocco <gio.fiocco2@gmail.com>
2025-12-16 15:26:12 +0100

    gol

--- examples/src/gol.c
+++ examples/src/gol.c
@@ -13,21 +13,56 @@ int grid1[SIZE] = {
 // clang-format on
 int grid2[SIZE];
 int *current_grid = grid1;
+int *back_grid = grid2;
 
 extern void c_put_char(char c);
+extern void c_print_int(int n);
+
+int at(int row, int col) {
+  if (row == -1 || row == N || col == -1 || col == N) {
+    return 0;
+  } else {
+    return current_grid[row * N + col];
+  }
+}
 
 void print_grid() {
-  // for (int i = 0; i != SIZE; i = i + 1) {
-  //   c_put_char((char)current_grid[i]);
-  // }
   for (int row = 0; row != N; row = row + 1) {
     for (int col = 0; col != N; col = col + 1) {
-      c_put_char((char)current_grid[row * N + col]);
+      c_put_char((char)at(row, col));
     }
     c_put_char(0x0A);
   }
 }
 
+void iteration() {
+  for (int row = 0; row != N; row = row + 1) {
+    for (int col = 0; col != N; col = col + 1) {
+      int n = at(row - 1, col - 1)
+              + at(row - 1, col)
+              + at(row - 1, col + 1)
+              + at(row, col - 1)
+              + at(row, col + 1)
+              + at(row + 1, col - 1)
+              + at(row + 1, col)
+              + at(row + 1, col + 1);
+      if (at(row, col)) {
+        back_grid[row * N + col] = n == 2 || n == 3;
+      } else {
+        back_grid[row * N + col] = n == 3;
+      }
+    }
+  }
+  int *tmp = current_grid;
+  current_grid = back_grid;
+  back_grid = tmp;
+}
+
 int main() {
   print_grid();
+
+  for (int i = 0; i != ITERATIONS; i = i + 1) {
+    iteration();
+    print_grid();
+  }
 }
